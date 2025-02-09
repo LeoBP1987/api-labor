5933db39c0946c1e0c59a9556bf71398a65ea117
LeoBP1987 <leonardobp1987@gmail.com>
2025-02-09 14:41:28 -0300

    Migrando BD de usuarios para Postgres

--- labor/settings.py
+++ labor/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 from mongoengine import connect
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -67,10 +68,11 @@ WSGI_APPLICATION = 'labor.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('URLPOSTGRES'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 # Conectando o MongoDB
